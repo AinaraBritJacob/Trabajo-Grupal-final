403026edd671261ac063b43a19c328e0c7910e99
AinaraBritJacob <166017112+AinaraBritJacob@users.noreply.github.com>
2024-11-10 18:55:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -16,3 +16,74 @@ M.onclick = function() {
 O.onclick = function() {
     L.innerHTML = ""; 
 };
+
+
+
+let Li= document.querySelector("#Li")
+let S= document.querySelector("#S")
+let A= document.querySelector("#A")
+
+S.onclick = function() {
+    Li.innerHTML = `
+       <li>2  huevos</li>
+<li>140 gr de azucar</li> 
+<li>100 cc aceite girasol</li>
+<li>50 cc leche</li>
+<li>160 gr harina leudante</li>
+<li>1 pizca de sal</li>
+<li>1 cdita escencia de vainilla</li>
+        `;
+}
+A.onclick = function() {
+    Li.innerHTML = ""; 
+};
+
+let Frutilla= document.querySelector("#Frutilla")
+let Fru= document.querySelector("#Fru")
+let Tilla= document.querySelector("#Tilla")
+
+Fru.onclick = function() {
+    Frutilla.innerHTML = `
+       <li>600g de frutillas congeladas</li>
+    <li>150 g de leche</li>
+        `;
+}
+Tilla.onclick = function() {
+    Frutilla.innerHTML = ""; 
+};
+
+let Leche= document.querySelector("#Leche")
+let Le= document.querySelector("#Le")
+let Che= document.querySelector("#Che")
+
+Le.onclick = function() {
+    Leche.innerHTML = `
+      <li>30ml de cafe</li>
+<li>60ml de leche</li>
+<li>1 cucharada de azucar(opcional)</li>
+        `;
+}
+Che.onclick = function() {
+    Leche.innerHTML = ""; 
+};
+
+
+
+let respuesta;
+ 
+let boton= document.querySelector("#Boton") 
+ 
+ 
+ boton.onclick= function () {
+   
+     respuesta = document.querySelector("#Inp").value;
+ 
+     if (respuesta == "Me gustaron") {
+         alert("Nos alegramos!");
+         
+     } else {
+         alert("Trabajaremos en mejorar");
+         
+     }
+ }
+ 
